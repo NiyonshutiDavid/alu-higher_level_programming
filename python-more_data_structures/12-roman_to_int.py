0be89b63ee4cbd1a2c73ccc2cdc621799cc304b3
NiyonshutiDavid <d.niyonshut@alustudent.com>
2023-11-07 06:29:00 -0800

    my own

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 
 
 def to_subtract(list_num):
+
+
     to_sub = 0
     max_list = max(list_num)
     for n in list_num:
@@ -9,6 +11,8 @@ def to_subtract(list_num):
             to_sub += n
     return (max_list - to_sub)
 def roman_to_int(roman_string):
+
+
     if not roman_string:
         return 0
     if not isinstance(roman_string, str):
